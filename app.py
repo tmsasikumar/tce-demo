5cd55dba92b2046a58cce69e43ac1ee6cd74b859
Sasi Kumar T <sasikt@thoughtworks.com>
2020-06-23 11:19:16 +0530

    Added addition functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
@@ -6,3 +6,15 @@ app=Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("calculator.html")
+
+
+def addNums(number1, number2):
+    total = number1 + number2
+    return str(total)
+
+
+@app.route("/", methods=["POST"])
+def add_handler():
+    number1 = int(request.form["num1"])
+    number2 = int(request.form["num2"])
+    return addNums(number1, number2)
